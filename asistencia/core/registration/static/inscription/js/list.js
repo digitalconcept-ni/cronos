585b502a1f7a5e83fd092d5b7ba9387e282f41a8
bryanguevara <bryanguevara38@gmail.com>
2024-12-12 22:35:08 -0600

    SE GUARDAN LO ARCHIVOS ANTES DEL MERGE

---
+++ asistencia/core/registration/static/inscription/js/list.js
@@ -0,0 +1,33 @@
+var inscription = {
+    config: [
+        {
+            targets: [0, 1, 2, 3, 4],
+            class: 'text-center',
+        },
+        {
+            targets: [-1],
+            orderable: false,
+            render: function (data, type, row) {
+                var buttons = '<a rel="edit" href="' + pathname + 'change/' + row[0] + '/" class="btn btn-warning btn-xs ml-1"> <i class="bi bi-pencil"></i></a> ';
+                buttons += '<a rel="delete" class="btn btn-danger btn-xs"><i class="bi bi-trash"></i></a>';
+                return buttons;
+            }
+        },
+    ],
+    list: function () {
+
+        let data = {
+            'data': {'action': 'search'},
+            'inserInto': 'rowList',
+            'th': ['Nro', 'Usuario', 'Carrera', 'Turno', 'Modalidad', 'Opciones'],
+            'table': 'tableList',
+            'config': inscription.config,
+            'modal': false,
+        }
+        drawTables(data);
+    }
+};
+
+$(function () {
+    inscription.list();
+});
